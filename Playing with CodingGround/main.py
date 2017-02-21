3cfcc277a7e33c0599cc5119e73257152c19dee1
hatani <hatani@gmail.com>
2017-02-21 10:53:02 +0100

    Update main.py

--- Playing with CodingGround/main.py
+++ Playing with CodingGround/main.py
@@ -5,7 +5,8 @@ import re
 greeting = 'Hello World!'
 EOL = '\n'
 
-regex = re.compile(r'Hella')
+regex = re.compile(r'Hell')
+print 'Searching in: ', greeting + EOL
 mo = regex.search(greeting)
 if mo:
     print '"Mo" is there!'
